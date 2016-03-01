cbbd09be9ad427ae385c1388d224453d0bfa3613
Kjetil Geirbo <kjetil@geirbo.no>
2016-03-01 18:54:51 +0100

    edit readme

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'telstar:videoframe',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'A port of the VideoFrame API to Meteor. There are only a small changes of the original code, witch is the ability to update values when video is seeked. This Readme has also been updated to reflect meteor usage.',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'A port of the VideoFrame API to Meteor. There are only a small changes of the original code, which is the ability to update values when video is seeked. This Readme has also been updated to reflect meteor usage.',
+  git: 'https://github.com/kjetilge/VideoFrame.git',
   documentation: 'README.md'
 });
 
